8aa0678c2135cd2f5c1ff287a83ddcfc16e0bd2d
Yu Kamiya <fuzzy31u@gmail.com>
2022-03-05 11:33:19 +0900

    Add Head component and fix script tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
-import Head from 'next/head'
+
 import Image from 'next/image'
 import Link from "next/link";
 import styles from '../styles/Home.module.css'
 import xml2js from "xml2js";
-import { rssUrl, siteDescription } from '../const';
+import { rssUrl } from '../const';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
-function initGA() {  
-  return {__html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-PXK0BSCDXQ');
-      gtag('config', 'UA-219601984-1');  
-  `}
-}
+import Head from '../components/Head';
 
 export async function getStaticProps() {
   const res = await fetch(rssUrl);
@@ -37,23 +27,7 @@ export async function getStaticProps() {
 export default function Home(item) {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>momit.fm</title>
-        <meta name="description" content={siteDescription} />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff"></meta>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" href="/apple-touch-icon.png" sizes="180x180"></link>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192"></link>
-        <link rel="icon" type="image/png" href="/android-chrome-512x512.png" sizes="512x512"></link>
-        <meta name="og:image" content="./sq-momitfm-gray.png"></meta>
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PXK0BSCDXQ"></script>
-        <script dangerouslySetInnerHTML={initGA()} />
-      </Head>
+      <Head></Head>
       <Header></Header>
       <div className={styles.main}>
           {
