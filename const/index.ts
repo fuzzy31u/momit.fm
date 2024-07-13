331a42aa3813f44deae2094b3b840be49d4e3ec4
Yu Kamiya <fuzzy31u@gmail.com>
2024-07-13 16:09:14 +0900

    fix: modify header description

--- const/index.ts
+++ const/index.ts
@@ -1,4 +1,3 @@
 export const siteDescription =
-  "IT企業で働くママエンジニアとママPMが、子育てやテックについてお話しするPodcastです.";
-// export const rssUrl = "https://anchor.fm/s/6d02f284/podcast/rss";
+  "「子育てをテクノロジーでハッピーに🌈」をコンセプトにおしゃべりする子育テックPodcast🎧";
 export const rssUrl = "https://rss.art19.com/momitfm";
